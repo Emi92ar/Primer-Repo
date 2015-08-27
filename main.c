5888097063c56bf43d7cb96225da87a6a0e5e3dc
Arian Giles García <arian2822@gmail.com>
2015-08-27 14:44:24 -0300

    Agregue funcion hola muno rama 2

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+void holaMundoRama2();
+
 int main(int argc, char **argv) {
 
 	printf("Hola GitHub!\n");
 
 	return 0;
+}
+
+void holaMundoRama2() {
+	printf("Hola a todo el mundo desde la rama 2!\n");
 }
