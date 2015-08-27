c34301b40fff3d51b50963cf8f2b5ae0ea7c0a62
mauriludemann <mauriludemann@gmail.com>
2015-08-27 14:43:54 -0300

    Agregado el Holamundo de la rama 1

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 
+void holamundoR1();
+
 int main(int argc, char **argv) {
 
 	printf("Hola GitHub!\n");
+	holamundoR1();
 
 	return 0;
+}
+
+void holamundoR1(){
+	printf("Hola Mundo Rama 1");
 }
